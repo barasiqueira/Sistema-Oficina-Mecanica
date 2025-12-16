ccd07f29e39a6888a55c2a9436d3f1c287d91339
barasiqueira <110621003+barasiqueira@users.noreply.github.com>
2025-12-16 23:10:14 +0100

    Create CodigoInvalidoException.java

---
+++ src/com/barasiqueira/util/CodigoInvalidoException.java
@@ -0,0 +1,8 @@
+package com.barasiqueira.util;
+
+public class CodigoInvalidoException extends RuntimeException{
+
+    public CodigoInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+}
