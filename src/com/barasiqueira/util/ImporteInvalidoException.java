6593071402a0491b668de8b72c970b160a91a211
barasiqueira <110621003+barasiqueira@users.noreply.github.com>
2025-12-16 23:10:53 +0100

    Create ImporteInvalidoException.java

---
+++ src/com/barasiqueira/util/ImporteInvalidoException.java
@@ -0,0 +1,8 @@
+package com.barasiqueira.util;
+
+public class ImporteInvalidoException extends RuntimeException {
+
+    public ImporteInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+}
