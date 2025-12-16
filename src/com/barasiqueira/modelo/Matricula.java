8e6ca893da96862299cc259fb7f59f5aaf2b7217
barasiqueira <110621003+barasiqueira@users.noreply.github.com>
2025-12-16 23:15:10 +0100

    Delete src/com/barasiqueira/modelo/Matricula.java

--- src/com/barasiqueira/modelo/Matricula.java
+++
@@ -1,9 +0,0 @@
-package com.barasiqueira.modelo;
-
-public record Matricula(String provincia, String numero, String letra) {
-
-    @Override
-    public String toString() {
-        return provincia + "-" + numero + "-" + letra;
-    }
-}
